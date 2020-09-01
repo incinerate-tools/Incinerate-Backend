b1b2195b76350745f2e3631a12b0059f15ebdf28
incinerate-tools <incinerate.tools@protonmail.com>
2020-08-31 22:04:18 -0700

    Frontend update, testing mobile

---
+++ public/precache-manifest.bdbfe84ef4afa5a420b581114eefd483.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd33f66444bede195563f6df5f1088a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62f2953b3d188094fce4",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "32b8219aa5c093461a40",
+    "url": "/static/css/main.b7f65661.chunk.css"
+  },
+  {
+    "revision": "62f2953b3d188094fce4",
+    "url": "/static/js/2.e1f46d30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1f46d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b8219aa5c093461a40",
+    "url": "/static/js/main.f27a132d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "75b4247fdd3b97d0e3b8e07b115673c2",
+    "url": "/static/media/Raleway-Regular.75b4247f.ttf"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6278f1aac5dafbd1fb13f965b8f2ebe7.js"
+  "/precache-manifest.bdbfe84ef4afa5a420b581114eefd483.js"
 );
 
 self.addEventListener('message', (event) => {
